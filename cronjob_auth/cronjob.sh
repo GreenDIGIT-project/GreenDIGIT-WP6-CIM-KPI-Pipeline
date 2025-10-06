9538f6ebaa39b3502451efe9bf073a87a31732ad
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2025-10-06 13:05:14 +0200

    Adjusting cron_job and manifest to work as a standalone serviec.

--- cronjob_auth/cronjob.sh
+++ cronjob_auth/cronjob.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-1 0 * * * cd /home/goncalo/GreenDIGIT-WP6-CIM-KPI-Pipeline/ && { echo "===== Run started at $(date '+\%Y-\%m-\%d \%H:\%M:\%S') ====="; ./scripts/start_cluster.sh; } >> /home/goncalo/GreenDIGIT-AuthServer/cronjob_auth/logs/start_cluster.log 2>&1
+# 1 0 * * * cd /home/goncalo/GreenDIGIT-WP6-CIM-KPI-Pipeline/ && { echo "===== Run started at $(date '+\%Y-\%m-\%d \%H:\%M:\%S') ====="; ./scripts/start_cluster.sh; } >> /home/goncalo/GreenDIGIT-WP6-CIM-KPI-Pipeline/cronjob_auth/logs/start_cluster.log 2>&1
+
+cd /home/goncalo/GreenDIGIT-WP6-CIM-KPI-Pipeline/ && { echo "===== Run started at $(date '+\%Y-\%m-\%d \%H:\%M:\%S') ====="; ./scripts/start_cluster.sh; } >> /home/goncalo/GreenDIGIT-WP6-CIM-KPI-Pipeline/cronjob_auth/logs/start_cluster.log 2>&1
--- scripts/start_cluster.sh
+++ scripts/start_cluster.sh
@@ -8,9 +8,8 @@ set -e
 python3 -m venv . 2>/dev/null || true
 . bin/activate
 
-cd auth_metrics_server
 pip install -r requirements.txt
-cd ..
+
 python3 tokens/get_bearer_token/get_bearer_token.py
 python3 tokens/get_wattprint_token/get_wattprint_token.py
 docker compose up -d --force-recreate --no-deps
