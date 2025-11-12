c9563adcc20116e0b8a1677009cdb3255e3af5fa
Gonçalo Ferreira <g.j.teixeiradepinhoferreira@uva.nl>
2025-11-12 12:53:33 +0000

    Fixing common certs and keys.

--- scripts/gen-mongo-key.sh
+++ scripts/gen-mongo-key.sh
@@ -2,6 +2,7 @@
 
 # This generates the secrets and certification for distributed communication between servers
 
+# 1) Run this on each server.
 LOCAL_CERT="./.cert"
 LOCAL_SECRET="./.secrets"
 
@@ -15,3 +16,12 @@ chmod 600 "$LOCAL_CERT/mongo.key" "$LOCAL_CERT/mongo.pem" "$LOCAL_CERT/ca.pem"
 # shared RS keyFile (generate once, copy to the other host)
 openssl rand -base64 756 > "$LOCAL_SECRET/mongo-keyfile"
 chmod 600 "$LOCAL_SECRET/mongo-keyfile"
+
+# 2) Then we must bundle and use the bundle-ca.pem:
+# Rename each of the mongo.crt -> serverA.mongo.crt
+# ... then:
+
+sudo cat "$LOCAL_CERT/serverA.mongo.crt" "$LOCAL_CERT/serverB.mongo.crt" > "$LOCAL_CERT/ca-bundle.pem"
+
+# 3) Ensure both the ca-bundle.pem and the ./.secrets/mongo-key are the same in both servers.
+# This is done manually for the moment, unfortunately. :)
