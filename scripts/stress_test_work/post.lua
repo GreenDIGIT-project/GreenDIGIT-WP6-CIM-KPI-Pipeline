1cba7d37ab7cba32c75032d94ec55cbee625fa95
Gonçalo Ferreira <g.j.teixeiradepinhoferreira@uva.nl>
2026-02-23 09:08:12 +0000

    Organising the scripts folder.

--- scripts/stress_test_work/post.lua
+++ scripts/stress_test_work/post.lua
@@ -1,4 +1,4 @@
 -- post.lua
 wrk.method = "POST"
-wrk.body   = io.open("../_test_requests/01_raw.json", "r"):read("*a")
+wrk.body   = io.open("../../_test_requests/01_raw.json", "r"):read("*a")
 wrk.headers["Content-Type"] = "application/json"
