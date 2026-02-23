a426a64b25103dc8daa33e7fbf143ebd735a904d
Gonçalo Ferreira <g.j.teixeiradepinhoferreira@uva.nl>
2026-02-23 12:33:35 +0000

    Updating landing page with Grafana + cleaning up scripts metrics examples.

--- scripts/batch_submit_cnr/batch_submit_cnr.sh
+++ scripts/batch_submit_cnr/batch_submit_cnr.sh
@@ -15,19 +15,18 @@ docker compose exec -it metrics-db \
      --type=json --out /dump/metrics.jsonl
 
 # Copy it to the local folder, outside docker.
-mkdir -p dump/mongo
-mkdir -p dump/sql_cnr
-docker cp $(docker compose ps -q metrics-db):/dump/metrics.jsonl ./dump/mongo/
+mkdir -p dump/01_mongo
+docker cp $(docker compose ps -q metrics-db):/dump/metrics.jsonl ./dump/01_mongo/
 
 # 2) Convert Mongo export -> CNR envelopes JSONL (filtered) (CIM-compatible)
-# $EMAILS="atsareg@in2p3.fr,kostashn@gmail.com,iglesias@ifca.unican.es,kdombek@man.poznan.pl"
+EMAILS="atsareg@in2p3.fr,kostashn@gmail.com,iglesias@ifca.unican.es,kdombek@man.poznan.pl"
 START="2025-08-01T00:00:00Z"
 END="2026-02-20T23:59:59Z"
 ./bin/python ./scripts/batch_submit_cnr/process_dump.py dump/mongo/metrics.jsonl \
-  --emails "iglesias@ifca.unican.es,kdombek@man.poznan.pl" \
+  --emails $EMAILS \
   --start $START \
   --end "$END" \
-  --out-dir dump/02_dump_processed_02
+  --out-dir dump/02_dump_processed
 
 # 2) One-time: install Postgres driver in this repo venv
 source bin/activate
@@ -35,8 +34,7 @@ pip install -q psycopg2-binary==2.9.10
 
 # 3) Direct-load envelopes into CNR Postgres (no HTTP)
 python3 scripts/batch_submit_cnr/load_envelopes_direct_cnr.py \
-  dump/02_dump_processed/envelopes_atsareg_in2p3_fr.jsonl \
-  dump/02_dump_processed/envelopes_kostashn_gmail_com.jsonl \
+  dump/02_dump_processed/*.jsonl \
   --batch-size 5000
 
 # Check number of entries from the raw data
@@ -44,44 +42,7 @@ wc -l dump/01_mongo/metrics.jsonl
 wc -l dump/02_dump_processed/envelopes_*.jsonl
 
 # Check PostgreSQL entries (see if it matches)
-PGPASSWORD="$CNR_POSTEGRESQL_PASSWORD" psql -h "$CNR_HOST" -p 5432 -U "$CNR_USER" -d "$CNR_GD_DB" -c "
+PGPASSWORD="$CNR_POSTEGRESQL_PASSWORD" \
+psql -h "$CNR_HOST" -p 5432 -U "$CNR_USER" -d "$CNR_GD_DB" -c "
 SELECT count(*) FROM monitoring.fact_site_event;
 "
-
-# ROOT_DIR=$(pwd)
-# ENV_FILE="$ROOT_DIR/.env"
-
-# if [ -f "$ENV_FILE" ]; then
-#   # shellcheck disable=SC1091
-#   source "$ENV_FILE"
-# else
-#   echo "Missing .env at $ENV_FILE" >&2
-#   exit 1
-# fi
-
-# : "${CNR_POSTGRESQL_PASSWORD:?CNR_POSTGRESQL_PASSWORD must be set in .env}"
-# : "${CNR_HOST:?CNR_HOST must be set in .env}"
-# : "${CNR_USER:?CNR_USER must be set in .env}"
-# : "${CNR_GD_DB:?CNR_GD_DB must be set in .env}"
-
-# # 1. Dump the current CIM MetricsDB data:
-
-# # docker compose exec -it metrics-db mongodump --db metricsdb --out /dump
-# docker compose exec -it metrics-db \
-#      mongoexport --db metricsdb --collection metrics \
-#      --type=json --out /dump/metrics.jsonl
-
-# # Copy it to the local folder, outside docker.
-# mkdir -p dump/mongo # in case you haven't yet.
-# mkdir -p dump/sql_cnr
-# docker cp $(docker compose ps -q metrics-db):/dump/metrics.jsonl ./dump/mongo/
-
-
-# python3 process_dump.py dump/mongo/metrics.jsonl \
-#   --emails 'atsareg@in2p3.fr,kostashn@gmail.com' \
-#   --start '2025-08-01T00:00:00Z' \
-#   --end   "$(date -u +%Y-%m-%dT%H:%M:%SZ)" \
-#   --out-dir dump/sql_cnr/
-
-# # Script to batch submit the metrics to SoBigData MetricsDB
-# PGPASSWORD="$CNR_POSTEGRESQL_PASSWORD" psql -h $CNR_HOST -p 5432 -U $CNR_USER -d $CNR_GD_DB
